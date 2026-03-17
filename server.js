05b97adeceba1abd92de51fe18f9834f62f39d68
Honii&Codex <zhang1qqcs@gmail.com>
2026-03-17 12:36:13 +0800

    Refactor for secure deployment and clean repository

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = Number(process.env.PORT) || 3001;
+const HOST = process.env.HOST || '127.0.0.1';
+const PUBLIC_DIR = path.join(__dirname, 'public');
 
-app.use(express.static(__dirname));
+app.use(express.static(PUBLIC_DIR, { index: false }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Stand Reminder server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Stand Reminder server running on http://${HOST}:${PORT}`);
 });
